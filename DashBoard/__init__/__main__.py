fbede7b5942832fc1549b223f0f0ffca5ee2058d
renanll <renan_ll@hotmail.com>
2021-12-09 17:19:38 -0300

    BUID: Contrução na parte do Dash inicial

--- DashBoard/__init__/__main__.py
+++ DashBoard/__init__/__main__.py
@@ -1,8 +1,111 @@
+
+# %%
 from dash import Dash
-from dash_html_components import H1
+from dash_html_components import Div, H1, P, H3
+from dash_core_components import Graph, Dropdown, Slider, Checklist
+from dash_core_components import Input as DCCInput
+from dash.dependencies import Input, Output
 
-app = Dash(__name__)
+from yfinance import download
+from datetime import datetime
+df = download('PETR4.SA', '2021-01-01', datetime.now(),
+              progress=False)
+df2 = download('PETR3.SA', '2021-01-01', datetime.now(),
+               progress=False)
+df3 = download('PBR', '2021-01-01', datetime.now(),
+               progress=False)
 
-app.layout = H1('Boas vindas')
+external_stylesheets = [
+    'https://unpkg.com/terminal.css@0.7.2/dist/terminal.min.css'
+    '/assets/style.css',
+]
+
+database = {
+    'index': df.index,
+    'menores': df.Close,
+    'maiores': df3.Close,
+    'bebes': df2.Close,
+}
+# elemento main #
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+# componente do dash #
+
+
+app.layout = Div(
+    children=[
+        DCCInput(id='meu_input', value='Potato'),
+        P(id='output'),
+
+        H1('Olá '),
+        P('Bem vindo ao dash'),
+        P('Bora codar'),
+        H3('Gráfico de Linha'),
+
+        Dropdown(
+            options=[
+                {'label': 'Menores', 'value': 'menores'},
+                {'label': 'Maiores', 'value': 'maiores'},
+                {'label': 'Bebes', 'value': 'bebes'},
+            ],
+            value='menores'
+        ),
+        Slider(
+            min=0,
+            max=10,
+            step=1,
+            value=5
+        ),
+        Checklist(
+            options=[
+                {'label': 'Menores', 'value': 'menores'},
+                {'label': 'Maiores', 'value': 'maiores'},
+                {'label': 'Bebes', 'value': 'bebes'},
+            ],
+            value=['bebes'],
+        ),
+        Graph(
+            config={'displayModeBar': False},
+            figure={
+                'data': [
+                    {
+                        'x': df.index,
+                        'y': df['Close'],
+                        'type': 'line',
+                        'name':'Close',
+                    },
+                    {
+                        'x': df.index,
+                        'y': df['Low'],
+                        'type': 'line',
+                        'name':'Low',
+                    },
+                ],
+                'layout':
+                    {
+                        'title': ' Gráfico de linha da PETR4',
+                        'paper_bgcolor': '#222225',
+                        'titlefont':
+                        {
+                            'size': 30,
+                            'color': '#e8e9ed',
+                        }
+                },
+            },
+        )
+    ]
+)
+
+
+@app.callback(
+    Output('output', 'children'),
+    Input('meu_input', 'value')  # children #
+)
+def meu_callback(meu_input):
+    print(f'Callback {meu_input}')
+    return meu_input
+
 
 app.run_server()
+#debug=True faz rodar automaticamente#
+
+# %%
